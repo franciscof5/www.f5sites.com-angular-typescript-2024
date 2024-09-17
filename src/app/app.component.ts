17331c3442eb53be254b5889c7e135b610e3610d
Francisco Matelli <tercmt-francisco@ftd.com.br>
2024-09-17 19:50:06 +0200

    ngOnInit load src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TranslocoModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'f5sites2';
+  title = 'f5sites';
+  
+  // Método que carrega o script main.js dinamicamente
+  private loadScript(url: string) {
+    const script = document.createElement('script');
+    script.src = url;
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  ngAfterViewInit() {
+    // Carrega o main.js após a view ser inicializada
+    this.loadScript('assets/js/main.js');
+  }
 }
+
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+// import { TranslocoModule } from '@jsverse/transloco';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [CommonModule, RouterOutlet, TranslocoModule],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'f5sites2';
+// }
