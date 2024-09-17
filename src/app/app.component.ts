1ff66796641fb92017086f60efdb86d7b671a09f
Francisco Matelli <tercmt-francisco@ftd.com.br>
2024-09-17 20:22:01 +0200

    separa com chatGPT header e funciona com o site com layout e ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,46 @@
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [CommonModule, RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'f5sites';
+  
+//   // Método que carrega o script main.js dinamicamente
+//   private loadScript(url: string) {
+//     const script = document.createElement('script');
+//     script.src = url;
+//     script.async = true;
+//     document.body.appendChild(script);
+//   }
+
+//   ngAfterViewInit() {
+//     // Carrega o main.js após a view ser inicializada
+//     this.loadScript('assets/js/main.js');
+//   }
+// }
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TranslocoModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'f5sites';
-  
-  // Método que carrega o script main.js dinamicamente
+  title = 'f5sites2';
+    // Método que carrega o script main.js dinamicamente
   private loadScript(url: string) {
     const script = document.createElement('script');
     script.src = url;
@@ -25,19 +53,3 @@ export class AppComponent {
     this.loadScript('assets/js/main.js');
   }
 }
-
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { RouterOutlet } from '@angular/router';
-// import { TranslocoModule } from '@jsverse/transloco';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [CommonModule, RouterOutlet, TranslocoModule],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent {
-//   title = 'f5sites2';
-// }
