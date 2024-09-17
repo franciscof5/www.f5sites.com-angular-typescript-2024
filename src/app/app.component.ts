808ee3f29ecdd57e8789790a98773b54f8e797b3
Francisco Matelli <tercmt-francisco@ftd.com.br>
2024-09-17 17:07:23 +0200

    nao rendeeriza container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,67 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component, inject, OnDestroy } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { TranslocoService } from "@jsverse/transloco";
+import { LangDefinition } from "@jsverse/transloco";
+import { Subscription, take } from "rxjs";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, RouterModule],
 })
-export class AppComponent {
-  title = 'f5sites';
-  
-  // Método que carrega o script main.js dinamicamente
-  private loadScript(url: string) {
-    const script = document.createElement('script');
-    script.src = url;
-    script.async = true;
-    document.body.appendChild(script);
+export class AppComponent implements OnDestroy {
+  service = inject(TranslocoService);
+  availableLangs = this.service.getAvailableLangs() as LangDefinition[];
+  private subscription: Subscription | null;
+
+  get activeLang() {
+    return this.service.getActiveLang();
   }
 
-  ngAfterViewInit() {
-    // Carrega o main.js após a view ser inicializada
-    this.loadScript('assets/js/main.js');
+  changeLang(lang: string) {
+    // Ensure new active lang is loaded
+    this.subscription?.unsubscribe();
+    this.subscription = this.service
+      .load(lang)
+      .pipe(take(1))
+      .subscribe(() => {
+        this.service.setActiveLang(lang);
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+    this.subscription = null;
   }
 }
+
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [CommonModule, RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'f5sites';
+  
+//   // Método que carrega o script main.js dinamicamente
+//   private loadScript(url: string) {
+//     const script = document.createElement('script');
+//     script.src = url;
+//     script.async = true;
+//     document.body.appendChild(script);
+//   }
+
+//   ngAfterViewInit() {
+//     // Carrega o main.js após a view ser inicializada
+//     this.loadScript('assets/js/main.js');
+//   }
+// }
