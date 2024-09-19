5474724220ed5ec3261accbcd72e28e884f92c48
Francisco Matelli <tercmt-francisco@ftd.com.br>
2024-09-19 16:43:05 +0200

    Conditional style for language selector, start implementing pt and sp

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,13 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { TranslocoHttpLoader } from './transloco-loader';
-import { provideTransloco } from '@jsverse/transloco';
+import { getBrowserLang, provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideTransloco({
         config: { 
           availableLangs: ['en', 'es', 'pt', 'it'],
-          defaultLang: 'en',
+          defaultLang: getBrowserLang() ? getBrowserLang() : 'en',
           // Remove this option if your application doesn't support changing language in runtime.
           reRenderOnLangChange: true,
           prodMode: !isDevMode(),
--- src/app/components/language-selector/language-selector.component.ts
+++ src/app/components/language-selector/language-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TranslocoService } from '@jsverse/transloco';
 
 @Component({
@@ -8,6 +8,9 @@ import { TranslocoService } from '@jsverse/transloco';
 })
 export class LanguageSelectorComponent {
   constructor(private translocoService: TranslocoService) {}
+
+  @Input() showTitle: boolean = true;
+  @Input() showButton: boolean = true;
   public languagesList: Array<
     Record<'imgUrl' | 'code' | 'name' | 'shorthand', string>
   > = [
@@ -15,19 +18,25 @@ export class LanguageSelectorComponent {
       imgUrl: '/assets/images/flags/English.png',
       code: 'en',
       name: 'English',
-      shorthand: 'ENG',
+      shorthand: 'EN',
     },
     {
       imgUrl: '/assets/images/flags/Italian.png',
       code: 'it',
       name: 'Italian',
-      shorthand: 'ITA',
+      shorthand: 'IT',
     },
     {
       imgUrl: '/assets/images/flags/Persian.png',
-      code: 'fa',
-      name: 'Persian',
-      shorthand: 'PER',
+      code: 'pt',
+      name: 'Portuguese',
+      shorthand: 'BR',
+    },
+    {
+      imgUrl: '/assets/images/flags/Persian.png',
+      code: 'sp',
+      name: 'Spanish',
+      shorthand: 'SP',
     },
   ];
   public changeLanguage(languageCode: string): void {
