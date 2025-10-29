465b1087822c022edb3443dad5b9cafbb16fca11
Francisco Matelli <francisco@f5sites.com>
2025-10-29 20:03:31 -0300

    zapbot v2

--- src/app/components/zapbot/zapbot.component.ts
+++ src/app/components/zapbot/zapbot.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
 import { TranslocoService } from '@jsverse/transloco';
+import { TranslocoModule } from '@jsverse/transloco';
+import { RouterModule, RouterLink } from '@angular/router';
+import { LanguageSelectorModule } from '../language-selector/language-selector.module';
+import { LeadFormComponent } from '../form_espocrm/lead-form.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
+  standalone: true,
+  imports: [
+    TranslocoModule,
+    LanguageSelectorModule,
+    RouterLink,
+    RouterModule,
+    LeadFormComponent,
+    FormsModule,
+    FooterComponent,
+    NavbarComponent
+  ],
   selector: 'app-zapbot',
   templateUrl: './zapbot.component.html',
   styleUrls: ['./zapbot.component.css']
 })
 export class ZapbotComponent {
-  constructor(private transloco: TranslocoService) {}
+  constructor(public translocoService: TranslocoService) {} 
+  title = 'f5sites';
+  language = "en";
+
+  get currentLang(): string {
+    return this.translocoService.getActiveLang();
+  }
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit"); 
+  }
 }
