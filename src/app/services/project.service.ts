8cbdc3ba49d88321774067c467ccb3076d378258
Francisco Matelli <francisco@f5sites.com>
2026-07-29 11:01:40 -0300

    feat: add skeleton loading UI and implement generic cache mechanism in project service

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -11,7 +11,6 @@ import {
   ProjectQueryParams
 } from '../models/projects';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -23,50 +22,82 @@ export class ProjectService {
   private projectsApiUrl =
     'https://script.google.com/macros/s/AKfycby22VxGgLCO0M8M7GmKZXOB05A6OIrjT2pbQjwnRAb9cHzW4SefIt-RI_Zj132NWTN8/exec';
 
+  /** Cache for every GET request */
+  private cache = new Map<string, Observable<any>>();
+
   constructor(private http: HttpClient) {}
-  
-  private projectsCache = new Map<string, Observable<ProjectsResponse>>();
 
-  getProjects(query: ProjectQueryParams = {}): Observable<ProjectsResponse> {
+  /**
+   * Generic cached GET
+   */
+  private cachedGet<T>(url: string, params?: HttpParams): Observable<T> {
+    const key = `${url}?${params?.toString() ?? ''}`;
 
-    const key = JSON.stringify(query);
+    if (!this.cache.has(key)) {
+      console.log('HTTP:', key);
 
-    if (!this.projectsCache.has(key)) {
+      const request = this.http.get<T>(url, { params }).pipe(
+        shareReplay({ bufferSize: 1, refCount: false })
+      );
 
-      let params = new HttpParams();
-
-      if (query.featured) params = params.set('featured', '1');
-      if (query.status) params = params.set('status', query.status);
-      if (query.type) params = params.set('type', query.type);
-      if (query.search) params = params.set('search', query.search);
-      if (query.lang) params = params.set('lang', query.lang);
-
-      const request = this.http
-        .get<ProjectsResponse>(this.projectsApiUrl, { params })
-        .pipe(shareReplay(1));
-
-      this.projectsCache.set(key, request);
+      this.cache.set(key, request);
+    } else {
+      console.log('CACHE:', key);
     }
 
-    return this.projectsCache.get(key)!;
+    return this.cache.get(key)!;
   }
 
-  clearCache() {
-    this.projectsCache.clear();
+  getProjects(query: ProjectQueryParams = {}): Observable<ProjectsResponse> {
+    let params = new HttpParams();
+
+    if (query.featured) params = params.set('featured', '1');
+    if (query.status) params = params.set('status', query.status);
+    if (query.type) params = params.set('type', query.type);
+    if (query.search) params = params.set('search', query.search);
+    if (query.refresh) params = params.set('refresh', '1');
+    if (query.lang) params = params.set('lang', query.lang);
+
+    return this.cachedGet<ProjectsResponse>(this.projectsApiUrl, params);
+  }
+
+  getProjectBySlug(
+    slug: string,
+    lang?: string
+  ): Observable<Project | ProjectErrorResponse> {
+
+    let params = new HttpParams().set('slug', slug);
+
+    if (lang) {
+      params = params.set('lang', lang);
+    }
+
+    return this.cachedGet<Project | ProjectErrorResponse>(
+      this.projectsApiUrl,
+      params
+    );
   }
 
   getStats(): Observable<ProjectStats> {
-    return this.http.get<ProjectStats>(`${this.statsApiUrl}?stats=1`);
-  }
-  
-  getProjectBySlug(slug: string, lang?: string): Observable<Project | ProjectErrorResponse> {
-    let params = new HttpParams().set('slug', slug);
-    if (lang) params = params.set('lang', lang);
+    const params = new HttpParams().set('stats', '1');
 
-    return this.http.get<Project | ProjectErrorResponse>(this.projectsApiUrl, { params });
+    return this.cachedGet<ProjectStats>(this.statsApiUrl, params);
   }
 
   getFeatured(lang?: string): Observable<ProjectsResponse> {
-    return this.getProjects({ featured: true, lang });
+    return this.getProjects({
+      featured: true,
+      lang
+    });
+  }
+
+  clearCache(): void {
+    this.cache.clear();
+  }
+
+  clearCacheByUrl(url: string): void {
+    [...this.cache.keys()]
+      .filter(key => key.startsWith(url))
+      .forEach(key => this.cache.delete(key));
   }
 }
