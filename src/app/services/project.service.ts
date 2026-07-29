8feb2d8b51a61cb73832098ef9e6dfbfc4651f5b
Francisco Matelli <francisco@f5sites.com>
2026-07-29 10:38:19 -0300

    feat: implement project request caching in service and update footer translations

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 
 import {
   Project,
@@ -23,24 +24,41 @@ export class ProjectService {
     'https://script.google.com/macros/s/AKfycby22VxGgLCO0M8M7GmKZXOB05A6OIrjT2pbQjwnRAb9cHzW4SefIt-RI_Zj132NWTN8/exec';
 
   constructor(private http: HttpClient) {}
+  
+  private projectsCache = new Map<string, Observable<ProjectsResponse>>();
+
+  getProjects(query: ProjectQueryParams = {}): Observable<ProjectsResponse> {
+
+    const key = JSON.stringify(query);
+
+    if (!this.projectsCache.has(key)) {
+
+      let params = new HttpParams();
+
+      if (query.featured) params = params.set('featured', '1');
+      if (query.status) params = params.set('status', query.status);
+      if (query.type) params = params.set('type', query.type);
+      if (query.search) params = params.set('search', query.search);
+      if (query.lang) params = params.set('lang', query.lang);
+
+      const request = this.http
+        .get<ProjectsResponse>(this.projectsApiUrl, { params })
+        .pipe(shareReplay(1));
+
+      this.projectsCache.set(key, request);
+    }
+
+    return this.projectsCache.get(key)!;
+  }
+
+  clearCache() {
+    this.projectsCache.clear();
+  }
 
   getStats(): Observable<ProjectStats> {
     return this.http.get<ProjectStats>(`${this.statsApiUrl}?stats=1`);
   }
-
-  getProjects(query: ProjectQueryParams = {}): Observable<ProjectsResponse> {
-    let params = new HttpParams();
-
-    if (query.featured) params = params.set('featured', '1');
-    if (query.status) params = params.set('status', query.status);
-    if (query.type) params = params.set('type', query.type);
-    if (query.search) params = params.set('search', query.search);
-    if (query.refresh) params = params.set('refresh', '1');
-    if (query.lang) params = params.set('lang', query.lang);
-
-    return this.http.get<ProjectsResponse>(this.projectsApiUrl, { params });
-  }
-
+  
   getProjectBySlug(slug: string, lang?: string): Observable<Project | ProjectErrorResponse> {
     let params = new HttpParams().set('slug', slug);
     if (lang) params = params.set('lang', lang);
